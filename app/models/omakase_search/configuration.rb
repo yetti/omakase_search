e4e66ef3154c4f3305689bcf95289f739d2a6247
Yetrina Battad <hello@yetti.io>
2023-05-12 22:48:58 +1000

    feat: implement search engine configuration

---
+++ app/models/omakase_search/configuration.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module OmakaseSearch
+  class Configuration
+    include ActiveModel::Model
+
+    attr_accessor :engine, :view_component
+
+    def initialize(engine:, view_component:)
+      @engine = engine
+      @view_component = view_component
+    end
+  end
+end
