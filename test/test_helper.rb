34d2a33132d59b1f7858af7bf6ba5da0945005cf
Yetrina Battad <hello@yetti.io>
2023-05-12 20:23:06 +1000

    refactor: move dummy app from /spec to /test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require_relative "../spec/dummy/config/environment"
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../spec/dummy/db/migrate", __dir__)]
+require_relative "../test/dummy/config/environment"
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
 require "rails/test_help"
 
